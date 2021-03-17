4d02a74b49e82d0067a5cea009d97569fafa2bcb
xpointersite@outlook.com <xpointersite@outlook.com>
2021-03-17 15:38:34 -0300

    Navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container class="flex">
+    <v-col>
+      <v-card height="500" width="275" class="mx-auto">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="#9b0b9b" v-bind="attrs" v-on="on" depressed dark>
+              <div class="btnText">
+                <v-icon class="iconText">mdi-plus</v-icon>
+                Adicionar
+              </div>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in addItems" :key="index">
+              <v-list-item-icon class="icons">
+                <v-icon size="18px">{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-title class="cardText">{{
+                item.title
+              }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-navigation-drawer stateless touchless clipped permanent width="275">
+          <v-list nav class="mainText">
+            <v-list-item v-for="item in items" :key="item.title" link>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dados do Projeto" },
+        { title: "Visão Geral" },
+        { title: "Propostas" },
+        { title: "Vendas" },
+        { title: "Documentos" },
+        { title: "Histórico" },
+      ],
+
+      addItems: [
+        { title: "Arquivo", icon: "mdi-folder-outline" },
+        { title: "Agendamento", icon: "mdi-calendar-month" },
+        { title: "E-mail", icon: "mdi-email-outline" },
+        { title: "Notas", icon: "mdi-alert-circle-outline" },
+        { title: "Proposta", icon: "mdi-file-document-outline" },
+        { title: "Tarefa", icon: "mdi-calendar-check-outline" },
+        { title: "Venda", icon: "mdi-cash-usd-outline" },
+        { title: "Documento", icon: "mdi-file" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.flex {
+  align-content: left;
+}
+
+.icons {
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.iconText {
+  cursor: pointer;
+  margin: 5px;
+}
+
+.btnText {
+  margin-right: 120px;
+  font-weight: 300;
+}
+
+.cardText {
+  cursor: pointer;
+  margin: -25px;
+  font-size: 14px;
+}
+
+.mainText {
+  margin-left: 40px;
+  font-size: 14px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  vuetify,
+  icons: {
+    iconfont: "mdi", // default - only for display purposes
+  },
   render: function (h) {
     return h(App);
   },
